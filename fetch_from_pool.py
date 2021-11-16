4ed7c54cc428ea0f1d1dad896d238691f4d9edc3
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-11-16 12:58:01 -0500

    Create fetch_from_pool.py

---
+++ fetch_from_pool.py
@@ -0,0 +1,9 @@
+import request
+cid: str
+character: str
+av: str
+p: str
+url = ('https://fybgame.top:8000/web-1.0.0/bilibili/search?cid=%s&match=%s&av=%s&p=%s',
+       (cid, character, av, p))
+re=requestes.post(url).text
+print(re)
