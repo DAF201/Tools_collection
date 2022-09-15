a7c557387d884617fd3b5e615f338ace92f29c93
DAF201 <daf201@blink-in.com>
2022-09-14 23:05:10 -0500

    Create template_check_type.cpp

---
+++ C++/template_check_type.cpp
@@ -0,0 +1,40 @@
+
+For char array, cstring, and string
+
+OUTPUT:c b a
+...
+
+template <typename T>
+bool is_sub(T &source_1)
+{
+    if (std::is_same<T, string>::value)
+    {
+        printf("a\n");
+    }
+}
+
+template <typename T>
+bool is_sub(T &&source_1)
+{
+    if (std::is_same_v<const char *, T>)
+    {
+        printf("b\n");
+    }
+}
+
+template <typename T, std::size_t N>
+bool is_sub(T (&)[N])
+{
+    if (std::is_same_v<const char, T>)
+    {
+        printf("c\n");
+    }
+}
+
+int main()
+{
+    string a = "0";
+    is_sub("a");
+    is_sub(a.c_str());
+    is_sub(a);
+}
