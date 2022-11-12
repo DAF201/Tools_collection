ec57d2f39cbe96ac4965f15f65ea7be92c6e8325
daf201 <daf201@blink-in.com>
2022-11-12 08:59:06 -0600

    Add files via upload

--- python/serial_port.py
+++ python/serial_port.py
@@ -1,35 +1,41 @@
-#!python
-import time
+import serial
+import serial.tools.list_ports
+from tkinter import *
+from tkinter import ttk
 import threading
-import serial #pyserial not serial
-import random
-
-ser = serial.Serial(
-    'COM4', timeout=None, baudrate=115000, xonxoff=False, rtscts=False, dsrdtr=False
-)
 
 
-def ser_read():
-    while True:
-        data_raw = ser.readline()
-        print(data_raw)
+class serial_port_x(serial.Serial):
+    def __init__(self, window, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.window = window
+        self.clear()
+        self.window.port_recv_buffer.insert(END, 'connected\n')
+        self.read_thread = threading.Thread(target=self.serial_read)
+        self.read_thread.start()
 
+    def serial_read(self):
+        while True:
+            try:
+                data_raw = self.readline()
+                if data_raw != b'':
+                    print(data_raw)
+                self.window.port_recv_buffer.insert(END, data_raw)
+                self.window.port_recv_buffer.see('end')
+            except Exception as e:
+                print(e)
+                break
 
-def ser_cmd():
-    while True:
-        if bool(random.getrandbits(1)):
-            ser.write(b'root\r')
-            time.sleep(1)
-            ser.write(b'boot\r')
+    def serial_send(self, cmd):
+        self.write(cmd.encode()+b'\r')
+
+    def clear(self, target='recv'):
+        if target == 'recv':
+            self.window.port_recv_buffer.delete("1.0", "end")
         else:
-            ser.write(b'root\r')
-            time.sleep(1)
-            ser.write(b'reboot\r')
-        time.sleep(random.randrange(1, 10))
+            self.window.port_send_buffer.delete("1.0", "end")
 
-
-read_thread = threading.Thread(target=ser_read)
-write_thread = threading.Thread(target=ser_cmd)
-
-read_thread.start()
-write_thread.start()
+    def __del__(self) -> None:
+        self.window.port_recv_buffer.insert(END, 'disconnected\n')
+        self.close()
+        return super().__del__()
