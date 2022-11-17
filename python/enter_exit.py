f1ee3b534f31975a2505e53dedf2a389afb0a444
daf201 <daf201@blink-in.com>
2022-11-16 19:56:10 -0600

    Create enter_exit.py

---
+++ python/enter_exit.py
@@ -0,0 +1,25 @@
+class Sample:
+    def __enter__(self):
+        print("In __enter__()")
+        return "Foo"
+
+    def __exit__(self, type, value, trace):
+        print("In __exit__()")
+        print(type, value, trace)
+
+
+def get_sample():
+    return Sample()
+
+
+with get_sample() as sample:
+    print("sample:", sample)
+
+# output
+# In __enter__()
+#sample: Foo
+# In __exit__()
+# None None None
+
+# so basically it will run __enter__ when this object created, run __exit__ when leaving this block of code
+# The type, value, and trace are for exception handling
