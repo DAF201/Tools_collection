df8707e4b2e50e0a42a958c5f2bcff85bc8b3e33
daf201 <daf201@blink-in.com>
2023-05-27 21:06:23 -0500

    Update function_pointer.cpp

--- C++/function_pointer.cpp
+++ C++/function_pointer.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <type_traits>
 using namespace std;
+typedef void (*void_void)();
+typedef void (*void_int)(int);
 void test()
 {
     cout << "test 01" << endl;
 }
-void call(void *func_ptr)
+void test(int num)
 {
-    void (*func)();
-    func = (void (*)())func_ptr;
-    func();
+    cout << num << endl;
+}
+template <typename T>
+void call(T func_ptr)
+{
+    if (is_same<T, void_void>::value)
+    {
+        void_void func;
+        func = (void_void)func_ptr;
+        func();
+    }
+    else
+    {
+        void_int func;
+        // func = (void (*)(int))func_ptr;
+        func = (void_int)func_ptr;
+        func(2);
+    }
 }
 int main()
 {
-    call((void *)&test);
+    call((void_void)&test);
+    call((void_int)&test);
     return 0;
 }
