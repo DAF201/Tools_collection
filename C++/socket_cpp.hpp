166bafb0a43ec5859bac476982484426b01af173
fzy-0429 <130481849+fzy-0429@users.noreply.github.com>
2023-11-07 18:00:22 -0600

    Update socket_cpp.hpp
    recv fixed, replaced vector with queue

--- C++/socket_cpp.hpp
+++ C++/socket_cpp.hpp
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <string>
 #include <thread>
-#include <vector>
+#include <queue>
 #include <windows.h>
 #define MAX_LENGTH 896
 using namespace std;
+struct send_pkg
+{
+    void *data;
+    int size;
+};
 class cpp_socket
 {
 public:
@@ -16,88 +21,80 @@ public:
         if (WSAStartup(sock_version, &WSA_data) != 0)
         {
             printf("%s", "WSAStartup error\n");
-            return;
+            exit(-1);
         }
 
         client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         if (client_socket == INVALID_SOCKET)
         {
             printf("%s", "error when creating socket\n");
-            return;
+            exit(-1);
         }
 
         server_address.sin_family = AF_INET;
-        server_address.sin_port = server_port;
-        server_address.sin_port = htons(920);
+        server_address.sin_port = htons(server_port);
         server_address.sin_addr.s_addr = inet_addr(server_ip.c_str());
 
         if (connect(client_socket, (sockaddr *)&server_address, sizeof(server_address)) == SOCKET_ERROR)
         {
             printf("%s", "connecting error\n");
-            return;
+            exit(-1);
         }
-        // start two threads to recv and send
-        recv_thread = thread(&cpp_socket::recv_package, this);
-        send_thread = thread(&cpp_socket::send_package, this);
+
+        // LIE thread header used unix lib, so dont work here
+
+        RECV_THREAD = thread(&cpp_socket::RECVING, this);
+        SEND_THREAD = thread(&cpp_socket::SENDING, this);
+
         printf("%s", "socket created\n");
     }
-    char *c_recv()
+    void S_send(void *data, int size)
     {
-        // get the last element(earliest) in vector, and remove it
-        char *data = recv_buffer.back();
-        recv_buffer.pop_back();
-        return data;
-    }
-
-    void c_send(void *data)
+        send_pkg new_pkg = {data, size};
+        send_queue.push(new_pkg);
+    };
+    void *S_recv()
     {
-        // insert to begin of the vector
-        send_buffer.push_back((char *)data);
-        return;
-    }
+        if (!recv_queue.empty())
+        {
+            void *data = recv_queue.front();
+            recv_queue.pop();
+            return data;
+        }
+        return nullptr;
+    };
 
 private:
     SOCKET client_socket;
     WORD sock_version;
     WSADATA WSA_data;
     sockaddr_in server_address;
-    vector<char *> recv_buffer;
-    vector<char *> send_buffer;
-    thread recv_thread;
-    thread send_thread;
+    thread RECV_THREAD;
+    thread SEND_THREAD;
+    queue<void *> recv_queue;
+    queue<send_pkg> send_queue;
 
-    void recv_package()
+    void SENDING()
     {
-        char buffer[1024];
-        while (1)
+        send_pkg data;
+        while (true)
         {
-            // recieve something
-            recv(client_socket, buffer, sizeof(buffer), 0);
-            // printf("%s", buffer);
-
-            // CRUSHED HERE TODO:FIX
-
-            // inert it to begin of the vector
-            recv_buffer.insert(recv_buffer.begin(), buffer);
-            Sleep(100);
-        }
-    }
-
-    void send_package()
-    {
-        while (1)
-        {
-            // something need to be send
-            if (send_buffer.size() != 0)
+            if (!send_queue.empty())
             {
-                // get the last element of send vector
-                char *data = send_buffer.back();
-                // remove that
-                send_buffer.pop_back();
-                // send
-                send(client_socket, data, strlen(data), 0);
+                data = send_queue.front();
+                send_queue.pop();
+                send(client_socket, (const char *)data.data, data.size, 0);
                 Sleep(100);
             }
+        }
+    }
+    void RECVING()
+    {
+        char buffer[1024];
+        while (true)
+        {
+            recv(client_socket, buffer, 1024, 0);
+            recv_queue.push(buffer);
         }
     }
 };
