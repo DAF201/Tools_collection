fecc4d316b1a9a152d53ddee5b3aa26064c6b91c
daf201 <daf201@blink-in.com>
2024-11-19 22:55:21 -0800

    Delete javascript/http_server.js

--- javascript/http_server.js
+++
@@ -1,8 +0,0 @@
-// nodejs, this is the shortest http server I ever seem so far...
-var http = require('http');
-
-http.createServer(
-    function (req, res) {
-        res.write('Hello World!');
-        res.end();
-    }).listen(8888);
