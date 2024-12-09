bce97f44aab79fb0631a54ffd660d931bff5ff0c
daf201 <daf201@blink-in.com>
2024-12-08 20:49:31 -0800

    fixed padding issue with large file

--- C++/b64_coding.hpp
+++ C++/b64_coding.hpp
@@ -1,137 +1,96 @@
 #ifndef CODING_HPP
 #define CODING_HPP
 
-#ifndef TOOLS_HPP
 #include <string>
-#include <string.h>
-#endif
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
 
-namespace clear_sky
+namespace data
 {
 #define byte unsigned char *
-    // repalcement of memset
-    void set_mem(void *p, char data, int size)
-    {
-        for (int i = 0; i < size; i++)
-            ((byte)p)[i] = data;
-    }
 
-    // convertion from int num to str num
+    // Mapping for encoding and decoding
     const static char int2str_map[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-    // dictionary
     const static char byte2char_map[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-";
 
-    // convert a byte to a visible char
-    static char byte2char(char c)
-    {
+    static char byte2char(char c) {
         return byte2char_map[c];
     }
 
-    // convert a visible chat to byte
-    static char char2byte(char c)
-    {
-        // hardcoded because I am lazy
-        if (c == '+')
-            return 62;
-        if (c == '-')
-            return 63;
-        if (c < 48 || c > 122)
-            return -1;
-        if (c < 58)
-            return c - 48;
-        if (c < 91)
-            return c - 55;
-        else
-            return c - 61;
+    static char char2byte(char c) {
+        if (c == '+') return 62;
+        if (c == '-') return 63;
+        if (c >= '0' && c <= '9') return c - '0';
+        if (c >= 'A' && c <= 'Z') return c - 'A' + 10;
+        if (c >= 'a' && c <= 'z') return c - 'a' + 36;
+        return -1; // Invalid character
     }
 
-    // chunk of bytes, IDK what was encoded so you need to typecast or copy it on your own
-    struct bytes
-    {
+    struct bytes {
         int size;
         byte data;
+
         void copy(void *dest) { memcpy(dest, data, size); }
-        ~bytes()
-        {
-            delete data;
+
+        ~bytes() {
+            free(data);
             data = NULL;
         }
     };
 
-    std::string encode(byte data, long unsigned int size)
-    {
+    std::string encode(byte data, long unsigned int size) {
+        // Pad data length to a multiple of 3
+        long unsigned int padded_length = (size + 2) / 3 * 3;
 
-        // pad to a length divided by 3
-        long int padded_length = size;
-        while (padded_length % 3 != 0)
-            padded_length++;
-
-        // get a space divided by 3 and filled with 0
+        // Create a padded copy of the input
         byte padded_data = (byte)malloc(padded_length);
-        set_mem(padded_data, 0, padded_length);
+        memset(padded_data, 0, padded_length);
+        memcpy(padded_data, data, size);
 
-        // number of padding bytes at front
-        // replaced string with stringstream for performance
-        std::stringstream res("");
-        res << (int2str_map[padded_length - size]);
+        std::ostringstream res;
+        res << int2str_map[padded_length - size]; // Store padding size
 
-        // convert 3 bytes of 8 bits to 4 bytes of 6 bits then use the index to allocate something
-        // in another word, we are making 24 bits from 3 bytes of 8 bits to 4 bytes of 6 bits(but still stored as regular bytes with 2 0s in the front),
-        // so the regular byte 0101 0101 will become xx xx01 and 00 0101 two bytes and so on
-        for (int i = 0; i < size; i = i + 3)
-        {
-            // & will extract the wanted bit at index
-            // | will concatenate bits to byte
-            // << and >> move things around
-
-            // new byte 0: 0b00 + byte 0[0:5]
-            // new byte 1: 0b00 + byte 0[6:7] + byte 1[0:3]
-            // new byte 2: 0b00 + byte 1[4:7] + byte 2[0:1]
-            // new byte 3: 0b00 + byte 2[2:7]
-            res << byte2char((data[i] & 0b11111100) >> 2) << byte2char((((data[i] & 0b00000011) << 4) | ((data[i + 1] & 0b11110000) >> 4))) << byte2char(((data[i + 1] & 0b00001111) << 2) | ((data[i + 2] & 0b11000000) >> 6)) << byte2char(data[i + 2] & 0b00111111);
+        for (unsigned int i = 0; i < padded_length; i += 3) {
+            res << byte2char((padded_data[i] & 0b11111100) >> 2);
+            res << byte2char(((padded_data[i] & 0b00000011) << 4) | ((padded_data[i + 1] & 0b11110000) >> 4));
+            res << byte2char(((padded_data[i + 1] & 0b00001111) << 2) | ((padded_data[i + 2] & 0b11000000) >> 6));
+            res << byte2char(padded_data[i + 2] & 0b00111111);
         }
+
+        free(padded_data);
         return res.str();
     }
 
-    // encoding for std::std::string only cause this thing has dynamic length and sizeof does not work on it
-    std::string encode(std::string data)
-    {
+    std::string encode(const std::string &data) {
         return encode((byte)data.c_str(), data.length());
     }
 
-    bytes decode(std::string data)
-    {
-        // length of padding bytes
-        long unsigned int pad_size = data[0] - 48;
-        byte decode_result = (byte)malloc((data.length() - 1) / 4 * 3);
+    bytes decode(const std::string &data) {
+        // Extract padding size from the first character
+        long unsigned int pad_size = data[0] - '0';
+        long unsigned int decoded_length = (data.length() - 1) / 4 * 3 - pad_size;
+
+        byte decoded_data = (byte)malloc(decoded_length);
+
+        int j = 0;
+        for (unsigned int i = 1; i < data.length(); i += 4) {
+            char b0 = char2byte(data[i]);
+            char b1 = char2byte(data[i + 1]);
+            char b2 = char2byte(data[i + 2]);
+            char b3 = char2byte(data[i + 3]);
+
+            decoded_data[j++] = (b0 << 2) | ((b1 & 0b00110000) >> 4);
+            if (j < decoded_length)
+                decoded_data[j++] = ((b1 & 0b00001111) << 4) | ((b2 & 0b00111100) >> 2);
+            if (j < decoded_length)
+                decoded_data[j++] = ((b2 & 0b00000011) << 6) | b3;
+        }
 
         bytes result;
-        // bytes size = (3/4)chatacter size + padding
-        result.size = ((data.length() - 1) / 4 * 3) - pad_size;
-
-        data = data.substr(1);
-        std::string buffer;
-        char char_buffer[3];
-
-        // for each 4 characters
-        for (int i = 0, j = 0; i < data.length(); i += 4)
-        {
-            // replaced with indexing for performance
-            // get substring
-            // buffer = data.substr(i, i + 4);
-            // decode to 3 bytes
-            char_buffer[0] = ((char2byte(data[i]) << 2) | (char2byte(data[i + 1]) & 0b00110000) >> 4);
-            char_buffer[1] = ((char2byte(data[i + 1]) << 4) | (char2byte(data[i + 2]) & 0b00111100) >> 2);
-            char_buffer[2] = ((((char2byte(data[i + 2])) & 0b00000011) << 6) | (char2byte(data[i + 3]) & 0b00111111));
-
-            // write to final result
-            for (int i = 0; i < 3; i++)
-                decode_result[j++] = char_buffer[i];
-        }
-        result.data = decode_result;
+        result.size = decoded_length;
+        result.data = decoded_data;
         return result;
     }
-
 }
 #endif
